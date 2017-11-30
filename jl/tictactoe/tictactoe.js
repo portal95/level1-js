44c449d3d2d57ddf76c6327745ceb17eceff0a6a
JL40 <32827095+JL40@users.noreply.github.com>
2017-11-30 13:46:16 +0900

    [JL] Update Tic Tac Toe html, css, js files (final draft) (#68)
    * First commit
    
    * 로또번호생성기의 html파일
    
    * 로또번호생성기의 js파일
    
    * 라이언 그리기 : 라이언그리기 js파일
    
    * 로또번호생성기 js파일 수정본
    
    * edited files
    
    * 라이언 html 추가
    
    * 라이언 js파일 수정(html과 연결되도록)
    
    * 라이언 html: 실수로 집어넣은 '다'글자 삭제함
    
    * 행맨 교재버전의 js파일
    
    * 파일명 lotto.html을 index.html으로 변경함
    
    * lotto.html을 삭제함
    
    * 행맨게임 js파일 코드 잘못된 부분 수정함
    
    * Implemnt html & js
    
    * [JL: 2017/11/20] Updated hangman.js file
    
    * [JL: 2017/11/20] Updated index.html for the hangman game.
    
    * Upload html file for the Tic Tac Toe project
    
    * Delete hangman.html
    
    * Upload Tic Tac Toe css file
    
    * Upload Tic Tac Toe js file
    
    * Update Tic Tac Toe html file
    
    * Update Tic Tac Toe css file
    
    * Update Tic Tac Toe js file

--- jl/tictactoe/tictactoe.js
+++ jl/tictactoe/tictactoe.js
@@ -1,3 +1,137 @@
+<<<<<<< HEAD
+var ticArr = [];
+var running = true;
+
+// generate an array for the game table 
+function table(arr) { 
+	for (var i = 0; i < 3; i++) {
+		ticArr[i] = [];
+		for(var j = 0; j < 3; j++){
+			ticArr[i][j] = " ";
+		}
+	}
+}
+
+table(ticArr);
+
+// get the game table shown on the page  
+function showTable(arr) {
+	for (var i = 0; i < 3; i++) {
+		for (var j = 0; j < 3; j++) {
+			var numbers = document.getElementById("n"+i+j);
+			numbers.innerHTML = arr[i][j];
+		}
+	}
+}
+
+showTable(ticArr);
+
+var p1 = {x: -1, y: -1};
+var p2 = {x: -1, y: -1};
+var count = 0;
+
+// click the position
+function ticClick(event) {
+
+	if(running){
+		var id = event.target.id;
+		var idArr = id.split("").map(Number);
+		idArr.shift();
+
+		count++;
+		if(count == 1){
+			p1.x = idArr[0];
+			p1.y = idArr[1];
+			oxChange(ticArr, p1.x, p1.y);
+			winCombo(ticArr, "O");
+			document.getElementById("n"+p1.x+p1.y).disabled = true;
+		} 
+		if(count == 2){
+			p2.x = idArr[0];
+			p2.y = idArr[1];
+			oxChange(ticArr, p2.x, p2.y);
+			count = 0;
+			winCombo(ticArr, "X");
+			document.getElementById("n"+p2.x+p2.y).disabled = true;
+		}
+		showTable(ticArr);		
+	} else {
+		return;
+	}	
+}
+
+// change the coordinate values to "O"s and "X"s
+function oxChange(arr, x1, y1){
+	if(count == 1){
+		arr[x1][y1] = "O";
+	}
+	if(count == 2){
+		arr[x1][y1] = "X";
+	}	
+}
+
+// win conditions 
+function winCombo(arr, str){
+
+	// check rows
+	if(arr[0][0] == str && arr[0][1] == str && arr[0][2] == str){
+		alert(str + " has won!!");
+	 	running = false;
+	}
+	if(arr[1][0] == str && arr[1][1] == str && arr[1][2] == str){
+		alert(str + " has won!!");
+	 	running = false;	
+	}
+	if(arr[2][0] == str && arr[2][1] == str && arr[2][2] == str){
+		alert(str + " has won!!");
+	 	running = false;
+	}
+
+	// check columns 
+	if(arr[0][0] == str && arr[1][0] == str && arr[2][0] == str){
+		alert(str + " has won!!");
+	 	running = false;
+	}
+	if(arr[0][1] == str && arr[1][1] == str && arr[2][1] == str){
+		alert(str + " has won!!");
+	 	running = false;
+	}
+	if(arr[0][2] == str && arr[1][2] == str && arr[2][2] == str){
+		alert(str + " has won!!");
+	 	running = false;
+	}
+
+	// check left diagonal
+	if(arr[0][0] == str && arr[1][1] == str && arr[2][2] == str){
+		alert(str + " has won!!");
+	 	running = false;
+	}
+	
+	// check right diagonal 
+	if(arr[0][2] == str && arr[1][1] == str && arr[2][0] == str){
+		alert(str + " has won!!");
+	 	running = false;
+	}
+
+}
+
+// replay the game 
+function replay() {
+	running = true;
+	table(ticArr);
+	showTable(ticArr);
+	btnWork();
+}
+
+// get the replay button clicked when a game is done 
+function btnWork(){
+	for(var i = 0; i < 3; i++){
+		for(var j = 0; j < 3; j++){
+			document.getElementById("n"+i+j).disabled = false;
+		}
+	}
+}
+=======
 var origBoard;
 const huPlayer = 'O';
 const aiPlayer = 'X';
@@ -60,4 +194,5 @@ function checkWin(board, player) {
 
 
 
+>>>>>>> honux/jl
 
